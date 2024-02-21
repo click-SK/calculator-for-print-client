66ff6fdf78bb90bd789b89d47f9d69780186d8ab
click-SK <sashakushnir4991@gmail.com>
2024-02-21 13:20:58 +0200

    урл

--- src/http/BaseUrl.js
+++ src/http/BaseUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:4444/api';
-// export const BASE_URL = 'https://calculator-for-print-server.onrender.com/api';
+// export const BASE_URL = 'http://localhost:4444/api';
+export const BASE_URL = 'https://calculator-for-print-server.onrender.com/api';
